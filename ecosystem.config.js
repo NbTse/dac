159ba1a844684066b1828cc0313240e731b5c932
nb685858 <nb685858@gmail.com>
2021-10-02 19:30:24 +0800

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: "dac-app",
+      mode: "cluster",
+      script: "/usr/bin/yarn",
+      args: "start",
+      watch: false,
+      instances: 4,
+      autorestart: true,
+      max_memory_restart: "300M",
+      env_development: {
+        PORT: 3003,
+        NODE_ENV: "development",
+      },
+      env_production: {
+        PORT: 3000,
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
